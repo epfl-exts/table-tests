3951fe88aa2691f65cd43d35850cdc342e0c2cce
SpiritualMachine <hrtanderson@gmail.com>
2018-12-21 11:35:05 +0100

    Modified tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { mount } from "enzyme";
 import renderer from "react-test-renderer";
 import Round, { Question } from "./Round";
 import Answer from "./Answer.js";
-import Controls from "./Controls.js";
+import Controls, { Buttons, Confirmation } from "./Controls.js";
 
 // describe("<Round />", () => {
 //   it("renders correctly", () => {
@@ -12,11 +12,25 @@ import Controls from "./Controls.js";
 //   });
 // });
 
-describe("<Round />", () => {
-  it("renders: Question, Answer and Controls", () => {
-    const wrapper = mount(<Round />);
-    expect(wrapper.contains(<Question />)).toEqual(true);
-    expect(wrapper.contains(<Answer />)).toEqual(true);
-    expect(wrapper.contains(<Controls />)).toEqual(true);
+// describe("<Round />", () => {
+//   const wrapper = mount(<Round />);
+
+//   it("renders: Question, Answer and Controls", () => {
+//     expect(wrapper.contains(<Question />)).toEqual(true);
+//     expect(wrapper.contains(<Answer />)).toEqual(true);
+//     expect(wrapper.contains(<Controls />)).toEqual(true);
+//   });
+
+//   // it("clicks predict and the confirmation pops-up", () => {
+//   //   expect(wrapper.contains(<Buttons />)).toEqual(true);
+//   //   expect(wrapper.contains(<Confirmation />)).toEqual(false);
+//   //   wrapper.find({ id: "predict" }).simulate("click");
+//   //   expect(wrapper.contains(<Confirmation />)).toEqual(true);
+//   // });
+// });
+
+describe("useTwoNumbers", ()=>{
+  it("renders some numbers", ()=>{
+    // test the useTwoNumbers hook here.
   });
 });
