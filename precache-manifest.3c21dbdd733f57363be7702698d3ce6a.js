0058caf635e3bcdb402f3748ba822b2c20445e16
Harry Anderson <harry@tsf-444-wpa-2-188.epfl.ch>
2019-01-16 13:43:22 +0100

    Updates

---
+++ precache-manifest.3c21dbdd733f57363be7702698d3ce6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c22bc7c3b15fe108c3f8",
+    "url": "/table-tests/static/js/runtime~main.c22bc7c3.js"
+  },
+  {
+    "revision": "d8eafea272825887c36d",
+    "url": "/table-tests/static/js/main.d8eafea2.chunk.js"
+  },
+  {
+    "revision": "1fdb2d7c87b68a21d6e3",
+    "url": "/table-tests/static/js/1.1fdb2d7c.chunk.js"
+  },
+  {
+    "revision": "d8eafea272825887c36d",
+    "url": "/table-tests/static/css/main.8e66effb.chunk.css"
+  },
+  {
+    "revision": "b35cb894d98c8428dd4847a6dde7ad1c",
+    "url": "/table-tests/index.html"
+  }
+];
